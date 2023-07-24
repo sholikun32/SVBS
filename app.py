2a505ad0646f1e2b3d42c78347681896e86c5879
sholikun32 <102804771+sholikun32@users.noreply.github.com>
2023-07-24 11:23:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template
 import streamlit as st
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
-@app.route('/streamlit')
-def streamlit():
-    st.set_page_config(page_title="My Streamlit App")
-    st.write("Hello, world!")
-if __name__ == '__main__':
-    app.run()
+def main():
+    st.set_page_config(
+        page_title="Streamlit Bootstrap Example",
+        layout="wide"
+    )
+    st.title("Streamlit Bootstrap Example")
+
+    # Add your Streamlit app content here
+    st.write("Welcome to the Streamlit Bootstrap Example!")
+    
+    # Create a Bootstrap-styled button
+    st.button("Click me", key="click_button", help="A Bootstrap-styled button")
+
+    # Use streamlit_bokeh_events to listen to button clicks
+    result = streamlit_bokeh_events(
+        events="click_button",
+        key="listen_button",
+        debounce_time=0,
+        override_height=75,
+        refresh_on_update=False,
+        override_width=200,
+    )
+
+    if result:
+        if "click_button" in result:
+            st.write("Button clicked!")
+
+if __name__ == "__main__":
+    main()
