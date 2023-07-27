c87c49c6ef9347eeae24c2182f4d5e35ab75efb1
sholikun32 <102804771+sholikun32@users.noreply.github.com>
2023-07-27 21:26:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,44 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Custom CSS for the login page
-custom_css = """
-<style>
-/* Add your custom CSS styles here */
-body {
-  background-color: #f4f4f4;
-}
+def login_page():
+    st.markdown("""
+    <style>
+    .login-container {
+        padding: 2rem;
+        max-width: 400px;
+        margin: auto;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+        background-color: #f9f9f9;
+    }
+    .input-field {
+        margin-bottom: 1rem;
+    }
+    .login-btn {
+        background-color: #007BFF;
+        color: white;
+        padding: 0.5rem 1rem;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
-.login-container {
-  max-width: 400px;
-  margin: 100px auto;
-  padding: 20px;
-  border-radius: 10px;
-  background-color: #ffffff;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-}
+    st.markdown('<h1 style="text-align:center;">Modern Login</h1>', unsafe_allow_html=True)
 
-.login-header {
-  text-align: center;
-  font-size: 24px;
-  margin-bottom: 20px;
-}
+    # Login Form
+    with st.form(key='login-form', class_='login-container'):
+        username = st.text_input('Username', key='username', class_='input-field')
+        password = st.text_input('Password', key='password', class_='input-field', type='password')
+        login_button = st.form_submit_button('Login', class_='login-btn')
 
-.login-input {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #cccccc;
-  border-radius: 5px;
-}
+    # Handle login functionality here
+    if login_button:
+        # Validate username and password
+        if username == 'your_username' and password == 'your_password':
+            st.success('Login successful!')
+        else:
+            st.error('Invalid credentials')
 
-.login-button {
-  width: 100%;
-  padding: 10px;
-  background-color: #007bff;
-  color: #ffffff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.login-button:hover {
-  background-color: #0056b3;
-}
-
-</style>
-"""
-
-# Custom HTML for the login form
-login_form = """
-<div class="login-container">
-  <div class="login-header">Login</div>
-  <input class="login-input" type="text" placeholder="Username">
-  <input class="login-input" type="password" placeholder="Password">
-  <button class="login-button">Log in</button>
-</div>
-"""
-
-def main():
-    st.title("Modern Streamlit Login")
-
-    # Inject custom CSS into the Streamlit app
-    components.html(custom_css)
-
-    # Inject the login form into the Streamlit app
-    components.html(login_form)
-
-if __name__ == "__main__":
-    main()
